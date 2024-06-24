68917d561049b56ddce5761ee9cd542b44979b0d
Nguyen Duc Tri <nguyenductri2k3@gmail.com>
2024-06-24 10:11:15 +0700

    AVP-55: Switch 1 address to multi address in Branch API

---
+++ app/Models/Address.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use App\Trait\Models\SwitchTimezoneTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Address extends Model
+{
+    use SwitchTimezoneTrait;
+
+    protected $hidden = [
+        'addressable_type',
+        'addressable_id',
+    ];
+
+    protected $appends = [
+        'address_preview',
+    ];
+
+    /**
+     * @return MorphTo
+     */
+    public function addressable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'default' => 'bool',
+        ];
+    }
+
+    /**
+     * @return Attribute
+     */
+    protected function addressPreview(): Attribute
+    {
+        return Attribute::get(
+            fn(): string => "$this->address_detail, $this->ward, $this->district, $this->province"
+        );
+    }
+}
--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use App\Trait\Models\SwitchTimezoneTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Branch extends Model
 {
     use SwitchTimezoneTrait;
+
+    protected $with = [
+        'addresses',
+    ];
+
+    /**
+     * @return MorphMany
+     */
+    public function addresses(): MorphMany
+    {
+        return $this->morphMany(Address::class, 'addressable');
+    }
 }
